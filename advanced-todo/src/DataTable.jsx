1bda359d08d2e92f20ff9054788469eab9f894e0
ankys8820 <140976749+ankys8820@users.noreply.github.com>
2024-06-13 22:57:26 +0530

    simple-todo app created with minimal functions

---
+++ advanced-todo/src/DataTable.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+
+const DataTable = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    gender: "",
+    age: "",
+  });
+  const [data, setData] = useState([]);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  //   console.log(formData);
+
+  const handleAddClick = () => {
+    if (formData.name && formData.gender && formData.age) {
+      const newItem = {
+        id: Date.now(),
+        name: formData.name,
+        gender: formData.gender,
+        age: formData.age,
+      };
+      setData([...data, newItem]);
+      setFormData({ name: "", gender: "", age: "" });
+      alert("Data added successfully!");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = data.filter((item) => item.id !== id);
+    setData(updatedList);
+    alert("Data removed!");
+  };
+
+  //   console.log(data);
+  return (
+    <>
+      <div className="container">
+        <div className="add-container">
+          <div className="info-container">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Gender"
+              name="gender"
+              value={formData.gender}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Age"
+              name="age"
+              value={formData.age}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button className="add" onClick={handleAddClick}>
+            ADD
+          </button>
+        </div>
+
+        <div className="search-table-container">
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="search-input"
+            value={""}
+            onChange={() => {}}
+          />
+          {/*  */}
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {data.map((item) => (
+                <>
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.gender}</td>
+                    <td>{item.age}</td>
+                    <td className="actions">
+                      <button className="edit">Edit</button>
+                      <button
+                        onClick={() => handleDelete(item.id)}
+                        className="delete"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+          <div className="pagination"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DataTable;
